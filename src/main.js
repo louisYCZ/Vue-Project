f892d0a4a1bc6231af15eb4a1e8002355ed8ed84
yu <904927302@qq.com>
2017-08-22 19:04:18 +0800

    项目基本结构搭建完成

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import App from './App.vue';
+import Home from './components/home/Home.vue'
+
+import MintUi from 'mint-ui';
+import  'mint-ui/lib/style.css'
+Vue.use(MintUi);
+
+import './static/libs/mui-master/dist/css/mui.css'
+
+
+import VueRouter from  'vue-router';
+Vue.use(VueRouter);
+let router=new VueRouter({
+  routes:[
+    {name:'home',path:'/home',component:Home}
+  ]
+});
+
+//Axios: 开始
+import Axios from 'axios';
+Vue.prototype.$ajax=Axios;
+Axios.defaults.baseURL='http://182.254.146.100:8899/api/';
+
+
+
+
+new Vue({
+  el:'#app',
+  router,
+  render:c=>c(App)
+})
